2665bcc00b8d09f0dbbe3ee0d1f6a9218cab5c0e
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-15 10:23:57 +0530

    initialized nest.js setup

--- src/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { signIn, signUp } from "../controllers/auth.controller.js";
-
-export const AuthRouter = express.Router();
-
-AuthRouter.post("/signup", signUp);
-AuthRouter.post("/signin", signIn);
